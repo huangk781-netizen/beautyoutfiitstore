8792079c861b866126040aef3b6d479f2e46cae7
huangk781 <huangk781@gmail.com>
2026-08-22 02:54:25 +0800

    Fix login after account registration

--- accounts/views.py
+++ accounts/views.py
@@ -17,7 +17,11 @@ def register(request):
         form = RegisterForm(request.POST)
         if form.is_valid():
             user = form.save()
-            auth_login(request, user)
+            auth_login(
+                request,
+                user,
+                backend='django.contrib.auth.backends.ModelBackend',
+            )
             return redirect(next_url or 'products:product_list')
     else:
         form = RegisterForm()
